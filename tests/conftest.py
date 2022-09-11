ae5c671e20fe39606866d79bad0811d7829763c2
n.rurenko <eavtorsky@gmail.com>
2022-09-11 17:16:26 +0300

    test(./tests/conftest.py): make conftest great again

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
 import os
-import sys
-
-root_dir = os.path.abspath(__file__)
-sys.path.append(root_dir)
 
 BASE_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'backend'
 )
 root_dir_content = os.listdir(BASE_DIR)
+print(BASE_DIR)
 
 PROJECT_DIR_NAME = 'foodgram_api'
 
